05737d2682477061b119272d7bd2ebaaf52c8afa
Tim Evens <tievens@cisco.com>
2022-03-04 07:27:23 -0800

    v2.1.0 updates
    * Add peeringdb script and cron job
    * Fix running more than one cronjob at a time
    * Update upgrade script for psql-app

--- psql-app/upgrade/upgrade_2.1.0.sh
+++ psql-app/upgrade/upgrade_2.1.0.sh
@@ -6,10 +6,18 @@
 # Author: Tim Evens <tim@evensweb.com>
 #
 
-. /usr/local/openbmp/pg_profile && psql -c "select * from l3vpn_rib limit 1" > /dev/null 2>&1
+source /usr/local/openbmp/pg_profile
+
+psql -c "select * from l3vpn_rib limit 1" > /dev/null 2>&1
 
 if [[ $? -ne 0 ]]; then
   echo "==> Upgrading L3VPN SQL ======================================= "
   psql < /usr/local/openbmp/database/10_l3vpn.sql
   echo "==> Done upgrading L3VPN SQL ================================== "
+
+  echo "==> Upgrading to 2.1.0 SQL ==================================== "
+  psql < /tmp/upgrade/upgrade_2.1.0.sql
+  echo "==> Done upgrading to 2.1.0 SQL ================================== "
 fi
+
+
